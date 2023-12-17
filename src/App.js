479833b514a12e9d136f85f4e176d1591493ae74
benlovesfoxes <97691798+benlovesfoxes@users.noreply.github.com>
2023-12-17 14:53:10 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,106 +1,49 @@
+// StartMenu.js
+const StartMenu = () => {
+  // Start Menu component logic
+};
+
+// Taskbar.js
+const Taskbar = () => {
+  // Taskbar component logic
+};
+
+// Window.js
+const Window = ({ children }) => {
+  // Window component logic
+};
+
+// SystemTray.js
+const SystemTray = () => {
+  // System Tray component logic
+};
+
+// FileExplorer.js
+const FileExplorer = () => {
+  // File Explorer component logic
+};
+
+// Settings.js
+const Settings = () => {
+  // Settings component logic
+};
+
+// App.js
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  height: 100vh;
-  font-family: Arial, sans-serif;
-`;
-
-const Window = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  width: 400px;
-  height: 300px;
-  background-color: #fff;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-`;
-
-const TitleBar = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-  padding: 8px;
-  background-color: #0078d7;
-  color: #fff;
-  border-top-left-radius: 5px;
-  border-top-right-radius: 5px;
-`;
-
-const Title = styled.h2`
-  margin: 0;
-`;
-
-const Content = styled.div`
-  flex: 1;
-  padding: 16px;
-`;
-
-const LoginForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 8px;
-  margin-bottom: 16px;
-`;
-
-const Button = styled.button`
-  padding: 8px 16px;
-  background-color: #0078d7;
-  color: #fff;
-  border: none;
-  border-radius: 5px;
-  cursor: pointer;
-`;
-
-const ResetPasswordLink = styled.a`
-  color: #0078d7;
-  text-decoration: none;
-  margin-top: 16px;
-  cursor: pointer;
-`;
+import StartMenu from './StartMenu';
+import Taskbar from './Taskbar';
+import SystemTray from './SystemTray';
+import FileExplorer from './FileExplorer';
+import Settings from './Settings';
+// ... other imports
 
 const App = () => {
-  const handleLogin = (event) => {
-    event.preventDefault();
-    // Handle login logic here
-  };
-
-  const handleResetPassword = () => {
-    // Handle reset password logic here
-  };
-
+  // App component logic, including routing and state management for different features
   return (
-    <Container>
-      <Window>
-        <TitleBar>
-          <Title>Windows 11 Login</Title>
-          <span>⊞</span>
-        </TitleBar>
-        <Content>
-          <LoginForm onSubmit={handleLogin}>
-            <Input type="text" placeholder="Username" />
-            <Input type="password" placeholder="Password" />
-            <Button type="submit">Login</Button>
-            <ResetPasswordLink onClick={handleResetPassword}>
-              Reset Password
-            </ResetPasswordLink>
-          </LoginForm>
-        </Content>
-      </Window>
-    </Container>
+    <div className="Windows11">
+      <Taskbar />
+      {/* ... other components like StartMenu, SystemTray, etc. */}
+    </div>
   );
 };
 
